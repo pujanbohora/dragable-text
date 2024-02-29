4feed7c6e78bb6cd162a9fa97887c00392d7d469
Pujan Bohora <bohorapawan@gmail.com>
2024-02-29 16:21:23 +0545

    textfield added

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Dragable'),
+      home: const MyHomePage(title: 'Draggable'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -31,10 +31,22 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double xAxisPosition = 0.0;
   double yAxisPosition = 0.0;
-  Offset _position = Offset(0, AppBar().preferredSize.height);
+  // Offset _position = Offset(0, AppBar().preferredSize.height);
 
   Offset textBorderPosition = Offset.zero;
-  double textFontSize = 20.0;
+  double textFontSize = 20.0; // Initial font size
+  double borderSize = 10;
+  bool isDragAbleItemDropped = false;
+
+  Offset dragWidgetPosition = Offset.zero;
+
+  //change draggable widget position
+  double xAxisDragPosition = 0.0;
+  double yAxisDragPosition = 0.0;
+
+  //controller
+  TextEditingController addTextController = TextEditingController();
+  var addTextOnChangeValue = "";
 
   @override
   Widget build(BuildContext context) {
@@ -46,59 +58,217 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Center(
           child: Stack(
             children: <Widget>[
+              //DragTarget
               Positioned(
-                top: yAxisPosition,
-                left: xAxisPosition,
-                child: GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onPanStart: (value) {
-                    setState(() {
-                      textBorderPosition = value.localPosition;
-                      textBorderPosition = Offset(value.localPosition.dx, value.localPosition.dy);
-                    });
-                  },
-                  onPanUpdate: (value) {
-                    setState(() {
+                top: null,
+                bottom: null,
+                left: null,
+                right: null,
+                child: Center(
+                  child: DragTarget(
+                    builder: (BuildContext context, List<dynamic> accepted,
+                        List<dynamic> rejected) {
+                      return Container();
+                    },
+                    onAccept: (Color color) {
+                      setState(() {
+                        isDragAbleItemDropped = true;
+                        print("VALUE UPDATED::");
+                      });
+                    },
+                  ),
+                ),
+              ),
 
-                      final dx =
-                          value.localPosition.dx - textBorderPosition.dx;
-                      final dy =
-                          value.localPosition.dy - textBorderPosition.dy;
+              //Show Draggable Widget on complete i.e; (textEditingController)
+              Positioned(
+                  top: dragWidgetPosition.dy - 56,
+                  left: dragWidgetPosition.dx,
+                  child: Opacity(
+                    opacity: isDragAbleItemDropped ? 1 : 0,
+                    child: Container(
+                      height: 100,
+                      width: 200,
+                      color: Colors.transparent,
+                      child: Visibility(
+                        visible: isDragAbleItemDropped,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      // isDragAbleItemDropped = false;
+                                    });
+                                  },
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.white),
+                                          color: Colors.red),
+                                      child: const Icon(Icons.close,
+                                          color: Colors.white)),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isDragAbleItemDropped = false;
+                                    });
+                                  },
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(color: Colors.white),
+                                          color: Colors.green),
+                                      child: const Icon(
+                                        Icons.check,
+                                        color: Colors.white,
+                                      )),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            TextFormField(
+                              controller: addTextController,
+                              onChanged: (text) {
+                                setState(() {
+                                  addTextOnChangeValue = text;
+                                });
+                              },
+                              readOnly: !isDragAbleItemDropped,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )),
 
-                      final dragLength = Offset(dx, dy).distance;
-
-                      print("textBorderPosition::${textBorderPosition}");
-                      print("dtextBorderPosition::${textBorderPosition}");
-                      print("dx: $dx, dy: $dy, dragLength: $dragLength");
-
-                      textFontSize = 20.0 + (dragLength * 0.02);
-
-                      print("FONT SIZE:::${textFontSize}");
-                    });
-                  },
-                  child: Container(
+              //Draggable
+              Positioned(
+                  bottom: 20,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 10),
-                    decoration: BoxDecoration(border: Border.all(width: 40)),
-                    child: GestureDetector(
-                      onLongPress: () {
-                        setState(() {
-                          _position = const Offset(0, 0);
-                        });
-                      },
-                      onPanUpdate: (details) {
-                        setState(() {
-                          if (_position.dy + details.delta.dy >
-                              AppBar().preferredSize.height) {
-                            _position += details.delta;
-                            xAxisPosition = _position.dx;
-                            yAxisPosition = _position.dy;
-                          }
-                        });
-                      },
-                      child: Text(
-                        'Drag the text on long press',
-                        style: TextStyle(fontSize: textFontSize),
+                        vertical: 10, horizontal: 10),
+                    child: Row(
+                      children: [
+                        Draggable(
+                          data: Colors.grey,
+                          feedback: Material(
+                            child: GestureDetector(
+                              onPanStart: (value) {},
+                              onPanUpdate: (value) {},
+                              child: Container(
+                                height: 50,
+                                width: 200,
+                                color: Colors.transparent,
+                                child: TextFormField(
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          childWhenDragging: const Text("Add Text"),
+                          child: const Text("Add Text"),
+                          onDragCompleted: () {
+                            setState(() {
+                              isDragAbleItemDropped = true;
+                              print(
+                                  "onDragCompleted::::${isDragAbleItemDropped}");
+                            });
+                          },
+                          onDragEnd: (position) {
+                            setState(() {
+                              dragWidgetPosition = position.offset;
+                              print("onDragCompleted::::${dragWidgetPosition}");
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  )),
+
+              Positioned(
+                // top: yAxisPosition,
+                // left: xAxisPosition,
+                top: dragWidgetPosition.dy - 40,
+                left: dragWidgetPosition.dx,
+                child: Visibility(
+                  visible: !isDragAbleItemDropped,
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onPanStart: (value) {
+                      setState(() {
+                        textBorderPosition = value.localPosition;
+                      });
+                    },
+                    onPanUpdate: (value) {
+                      setState(() {
+                        final dx =
+                            value.localPosition.dx - textBorderPosition.dx;
+                        final dy =
+                            value.localPosition.dy - textBorderPosition.dy;
+                        final dragLength = Offset(dx, dy).distance;
+
+                        // Determine the direction of the drag
+                        final dragDirection = Offset(dx, dy) / dragLength;
+
+                        // Determine whether to increase or decrease font size based on drag direction
+                        final scaleFactor = dragDirection.dy > 0 ? 0.02 : -0.02;
+
+                        // Adjust font size accordingly
+                        textFontSize += dragLength * scaleFactor;
+
+                        // Ensure font size doesn't go below a minimum value
+                        textFontSize = textFontSize.clamp(10.0, 50.0);
+                        print("FONT SIZE:::$textFontSize");
+                      });
+                    },
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.resizeUpDown,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: borderSize)),
+                        child: GestureDetector(
+                          onDoubleTap: () {
+                            setState(() {
+                              print("dopuble tapped:::");
+                              // dragWidgetPosition = const Offset(0, 0);
+                              isDragAbleItemDropped = !isDragAbleItemDropped;
+                            });
+                          },
+                          onPanUpdate: (details) {
+                            setState(() {
+                              if (dragWidgetPosition.dy + details.delta.dy >
+                                  AppBar().preferredSize.height) {
+                                dragWidgetPosition += details.delta;
+                                xAxisPosition = dragWidgetPosition.dx;
+                                yAxisPosition = dragWidgetPosition.dy;
+                              }
+                            });
+                          },
+                          child: MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: Text(
+                              addTextOnChangeValue,
+                              style: TextStyle(fontSize: textFontSize),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -110,16 +280,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-  bool _isOnBorder(Offset tapPosition, BuildContext context) {
-    final RenderBox box = context.findRenderObject() as RenderBox;
-    final Size size = box.size;
-    const double borderWidth = 40.0;
-
-    return tapPosition.dx <= borderWidth ||
-        tapPosition.dx >= size.width - borderWidth ||
-        tapPosition.dy <= borderWidth ||
-        tapPosition.dy >= size.height - borderWidth;
-  }
-
 }
